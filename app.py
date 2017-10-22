84fb773cf4f5aa17318921b06c7d2b41810a5625
Cristina <ce@tsl.io>
2017-10-22 04:01:12 -0700

    remove test function call

--- app.py
+++ app.py
@@ -78,8 +78,6 @@ def lowerHardCodedItems():
 	print(f'Move those products. {mudBone}.')
 	return statement(f'Move those products. {mudBone}.')
 
-lowerHardCodedItems()
-
 if __name__ == '__main__':
 	port = int(os.environ.get("PORT", 443))
 	app.run(debug=True, host='0.0.0.0', port=port)
