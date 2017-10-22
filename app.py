3c914a85f5fb790673b4179a83d50ef52d62ec35
Cristina <ce@tsl.io>
2017-10-21 22:05:03 -0700

    trying more

--- app.py
+++ app.py
@@ -38,5 +38,5 @@ response = requests.request("GET", openLineItemsUrl, headers=headers)
 print("hello")
 
 if __name__ == '__main__':
-	port = int(os.environ.get("PORT", 5000))
+	port = int(os.environ.get("PORT", 443))
 	app.run(debug=True, host='0.0.0.0', port=port)
