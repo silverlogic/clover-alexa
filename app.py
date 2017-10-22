29c98d3726753b0fb17208e134f2778bdf349cc6
Cristina <ce@tsl.io>
2017-10-22 00:21:21 -0700

    get next items

--- app.py
+++ app.py
@@ -10,20 +10,26 @@ app = Flask(__name__)
 ask = Ask(app, '/')
 headers = { 'authorization': "Bearer 1669926f-c367-6f58-5027-31512f1661eb",'cache-control': "no-cache"}
 
+nextOpenOrderUrl = "https://api.clover.com/v3/merchants/3S2JC4YEV2XTE/orders?limit=1&status=open"
+BASE_URL = "https://api.clover.com/v3/merchants/3S2JC4YEV2XTE/"
 lineItems = []
 
 @ask.intent('getNextItems')
-def getLineItems():
-	elements = requests.request("GET", openLineItemsUrl, headers=headers).json()['elements']
-	for element in elements:
+def getNextLineItems():
+	orderID = getNextOrder()
+	openLineItemsUrl = f'{BASE_URL}orders/{orderID}/line_items'
+	lineItem_names = set()
+	elements = requests.request("GET", openLineItemsUrl, headers=headers).json()
+	for element in elements['elements']:
 		lineItems.append(element['name'])
-    	lineItems_str = ', '.join(lineItems[:-1])
-    	lineItems_str += f', and {lineItems[-1]}' 
+	lineItem_names = list(lineItem_names)
+	lineItems_str = ', '.join(lineItems[:-1])
+	lineItems_str += f', and {lineItems[-1]}' 
+	print(lineItems_str)
 	return statement(f'The items in the next order are {lineItems_str}') 
 
-nextOpenOrderUrl = "https://api.clover.com/v3/merchants/3S2JC4YEV2XTE/orders?limit=1&status=open"
-openLineItemsUrl = "https://api.clover.com/v3/merchants/3S2JC4YEV2XTE/orders/GQV74AGX3NEG0/line_items"
-
+def getNextOrder():
+	return requests.request("GET", nextOpenOrderUrl, headers=headers).json()['elements'][0]['id']
 
 # Hey Alexa, how many orders have we closed today? 
 
@@ -31,12 +37,10 @@ openLineItemsUrl = "https://api.clover.com/v3/merchants/3S2JC4YEV2XTE/orders/GQV
 
 # Hey Alexa, what's in the next open order?
 
-
-
-response = requests.request("GET", openLineItemsUrl, headers=headers)
-
-print("hello")
+# response = requests.request("GET", openLineItemsUrl, headers=headers)
+# getLineItems()
 
 if __name__ == '__main__':
-	port = int(os.environ.get("PORT", 443))
-	app.run(debug=True, host='0.0.0.0', port=port)
+	# port = int(os.environ.get("PORT", 443))
+	# app.run(debug=True, host='0.0.0.0', port=port)
+	app.run()
