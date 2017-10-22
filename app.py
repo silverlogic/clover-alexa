0b3b1f77479dbe3a504e37e53ff9c74ff9052631
Cristina <ce@tsl.io>
2017-10-21 21:12:28 -0700

    crash fix attempt

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ def getLineItems():
 	elements = requests.request("GET", openLineItemsUrl, headers=headers).json()['elements']
 	for element in elements:
 		lineItems.append(element['name'])
-    # lineItems_str = ', '.join(merchant_names[:-1]) #this is wrong fix it
-    # lineItems_str += f', and {merchant_names[-1]}' #
+    lineItems_str = ', '.join(lineItems[:-1])
+    lineItems_str += f', and {lineItems[-1]}' 
 	return statement(f'The items in the next order are ') #{lineItems_str}') 
 
 nextOpenOrderUrl = "https://api.clover.com/v3/merchants/3S2JC4YEV2XTE/orders?limit=1&status=open"
@@ -34,7 +34,7 @@ openLineItemsUrl = "https://api.clover.com/v3/merchants/3S2JC4YEV2XTE/orders/GQV
 
 response = requests.request("GET", openLineItemsUrl, headers=headers)
 
-print(getLineItems())
+print("hello")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host= '0.0.0.0')
